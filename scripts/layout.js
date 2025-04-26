d8342d4af8de0dfcadb444c6719fcca82fd53242
Ekrem Bülbül <ekrem.bulbul@visiott.com>
2025-04-26 22:32:22 +0300

    TURKCELEŞME DEVAM

--- scripts/layout.js
+++ scripts/layout.js
@@ -7,3 +7,23 @@ fetch('includes/header.html')
 fetch('includes/footer.html')
 .then(response => response.text())
 .then(data => document.getElementById('footer').innerHTML = data);
+
+
+var productLinks = document.getElementById("productLinks");
+if(productLinks){
+    
+fetch('includes/productLinks.html')
+.then(response => response.text())
+.then(data => {
+    productLinks.innerHTML = data;
+    const currentPage = window.location.pathname.split('/').pop(); 
+    console.log(currentPage); 
+    const links = productLinks.querySelectorAll('a'); 
+
+    links.forEach(link => {
+        if (link.getAttribute('href').includes(currentPage)) {
+            link.parentElement.classList.add('active'); 
+        }
+    });
+});
+}
