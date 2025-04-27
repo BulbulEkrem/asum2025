d08c7cb95394ab326d2b6ab443246dc450211dc0
Ekrem Bülbül <ekrem.bulbul@visiott.com>
2025-04-28 02:54:30 +0300

    hataların bazıları giderildi

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,7 +1,34 @@
 // Header'ı yükle
 fetch('includes/header.html')
 .then(response => response.text())
-.then(data => document.getElementById('header').innerHTML = data);
+.then(data => {
+    document.getElementById('header').innerHTML = data;
+    // Header yüklendikten sonra mobil menü için gerekli JavaScript kodlarını çalıştır
+    if (jQuery().superfish) {
+        jQuery('ul.sf-menu').superfish({
+            delay:       300,
+            animation:   {opacity:'show'},
+            animationOut: {opacity: 'hide'},
+            speed:       'fast',
+            disableHI:   false,
+            cssArrows:   true,
+            autoArrows:  true
+        });
+    }
+
+    //toggle mobile menu
+    jQuery('.toggle_menu').on('click', function(){
+        jQuery('.toggle_menu').toggleClass('mobile-active');
+        jQuery('.page_header').toggleClass('mobile-active');
+    });
+
+    jQuery('.mainmenu a').on('click', function(){
+        if (!jQuery(this).hasClass('sf-with-ul')) {
+            jQuery('.toggle_menu').toggleClass('mobile-active');
+            jQuery('.page_header').toggleClass('mobile-active');
+        }
+    });
+});
 
 // Footer'ı yükle
 fetch('includes/footer.html')
@@ -19,6 +46,44 @@ fetch('includes/productLinks.html')
     const currentPage = window.location.pathname.split('/').pop(); 
     console.log(currentPage); 
     const links = productLinks.querySelectorAll('a'); 
+
+    links.forEach(link => {
+        if (link.getAttribute('href').includes(currentPage)) {
+            link.parentElement.classList.add('active'); 
+        }
+    });
+});
+}
+
+var sectorLinks = document.getElementById("sectorLinks");
+if(sectorLinks){
+    
+fetch('includes/sectorLinks.html')
+.then(response => response.text())
+.then(data => {
+    sectorLinks.innerHTML = data;
+    const currentPage = window.location.pathname.split('/').pop(); 
+    console.log(currentPage); 
+    const links = sectorLinks.querySelectorAll('a'); 
+
+    links.forEach(link => {
+        if (link.getAttribute('href').includes(currentPage)) {
+            link.parentElement.classList.add('active'); 
+        }
+    });
+});
+}
+
+var solutionsLinks = document.getElementById("solutionsLinks");
+if(solutionsLinks){
+    
+fetch('includes/solutionsLinks.html')
+.then(response => response.text())
+.then(data => {
+    solutionsLinks.innerHTML = data;
+    const currentPage = window.location.pathname.split('/').pop(); 
+    console.log(currentPage); 
+    const links = solutionsLinks.querySelectorAll('a'); 
 
     links.forEach(link => {
         if (link.getAttribute('href').includes(currentPage)) {
